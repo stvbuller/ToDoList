0ad3df8193ff0ac0f5a20f8362cc6cefb9a84c06
Steven Buller <steven@Stevens-MacBook-Pro.local>
2015-11-20 19:12:22 -0500

    added delete button

--- jscripts/dom.js
+++ jscripts/dom.js
@@ -2,27 +2,35 @@ $(document).ready(function(){
 
 $(".btn-default").on("click", function(preventSubmit){
   preventSubmit.preventDefault();
-  alert("The button works");
 
 
-var newItem = $("#newItemText").val();
-console.log(newItem);
+    var newText = $("#newInput").val();
+    console.log(newText);
 
 
+    var newTableRow = $("<tr>");
+    var newTableData = $("<td>").append(newText);
+    var buttonDelete = $("<button>").addClass("btn btn-danger").append("Remove");
+    var deleteTableData = $("<td>").append(buttonDelete);
 
-});
+    var markedOFF = $("<checkbox>").append("Done");
+    var markedItem = $("<td>").append(markedOFF);
 
 
-/*
-var newTableRow = $("<tr>");
-var newTableData = $("<td>");
-var buttonDelete = $("button").addClass("btn btn-danger").append("Remove");
-var deleteTableData = $("<td>").append(buttonDelete);
-var checkBox = $("<checkbox>");
-var checkedTabelData = $(<td>).append(checkBox);
+    newTableRow.append(newTableData);
+    newTableRow.append(buttonDelete);
+    newTableRow.append(markedItem);
 
-*/
+    $("tbody").append(newTableRow);
+    $("#newInput").val("").focus;
 
+
+  });
+
+  $("table").on("click", ".btn-danger", function() {
+    $(this).parent().remove();
+
+  });
 
 
 });
