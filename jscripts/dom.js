ee4d828e112a82eb3d3254be6763a53844bab045
Steven Buller <steven@Stevens-MacBook-Pro.local>
2015-11-22 11:06:33 -0500

    finished input logic

--- jscripts/dom.js
+++ jscripts/dom.js
@@ -5,29 +5,29 @@ $(".alert-danger").hide();
 $(".btn-default").on("click", function(preventSubmit){
   preventSubmit.preventDefault();
 
+  $(".alert-danger").hide();
+
   var newText = $("#newInput").val().trim();
   console.log(newText);
 
-
-/*
   var isDup = false;
+    $("td.checkForDup").each(function() {
+      if($(this).text().trim() === newText) {
+        isDup = true;
+        return;
+      }
+    });
 
-
-  $("td.word-td").each(function () {    //check for duplicates
-    if ($(this).text().trim() === newText) {
-      isDup = true;
+    if(isDup) {
+      $(".alert-danger").show();
+      $("#newInput").val("").focus(); 
       return;
     }
-  });
   
-  if (isDup = true){
-    $(".alert-danger").show();
-    return;
-  }
-  */
+
 
   var newTableRow = $("<tr>");
-  var newTableData = $("<td>").addClass("word-td").append(newText);
+  var newTableData = $("<td>").addClass("checkForDup").append(newText);
   var buttonDelete = $("<button>").addClass("btn btn-danger").append("Remove");
   var deleteTableData = $("<td>").append(buttonDelete);
 
@@ -40,13 +40,17 @@ $(".btn-default").on("click", function(preventSubmit){
   newTableRow.append(markedItem);
 
   $("tbody").append(newTableRow);
-  $("#newInput").val("").focus;
-
+  $("#newInput").val("").focus();
   });
 
   $("table").on("click", ".btn-danger", function() {
     $(this).parent().remove();
+  });
 
+
+  $("form").on("click", ".alert-danger", function() {
+    $(".alert-danger").hide();
+    $("#newInput").val("").focus();
   });
 
 
